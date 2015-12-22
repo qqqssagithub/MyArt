dd49d5a9ab3c1b1dc2d43a45fd32ad14c4bc9760
csw <csw@test.com>
2015-12-22 19:41:35 +0800

    MyArt

---
+++ MyArt/MyMusicTest/Recommended/Views/MainTableViewCell.h
@@ -0,0 +1,22 @@
+//
+//  MainTableViewCell.h
+//  MyMusicTest
+//
+//  Created by qianfeng0 on 15/12/8.
+//  Copyright © 2015年 陈少文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "SongModel.h"
+#import "SearchModel.h"
+
+@interface MainTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *title;
+@property (weak, nonatomic) IBOutlet UILabel *author;
+
+- (void)updateWithModel:(SongModel *)model;
+
+- (void)updateWithSearchModel:(SearchModel *)model;
+
+@end
---
+++ MyArt/MyMusicTest/ViewController.h
@@ -0,0 +1,16 @@
+//
+//  ViewController.h
+//  MyMusicTest
+//
+//  Created by qianfeng0 on 15/12/5.
+//  Copyright © 2015年 陈少文. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+
+@end
+
